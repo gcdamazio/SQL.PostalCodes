4ec8022800323e555fb9bfd9835dfbe2f381ff85
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-29 13:17:01 -0500

    DONE exercise3.sql

--- answers/exercise3.sql
+++ answers/exercise3.sql
@@ -1,3 +1,10 @@
 Write a Query that returns all the Counties by state, as a concatenated string.
 
 mysql> SELECT state, GROUP_CONCAT(county ORDER BY county SEPARATOR ', ') AS counties FROM zipcodes GROUP BY state ORDER BY state;
+
+FIXING DATA
+  
+Write a Query that finds the correct value for the missing timezones based on shared city, state or city, state, and county.
+
+mysql> SELECT city,state,county, timezone, COUNT(*) AS instance_count FROM zipcodes WHERE timezone IS NULL OR timezone = FALSE GROUP BY city, state, county,timezone ORDER BY city,state, county, instance_count DESC;
+
