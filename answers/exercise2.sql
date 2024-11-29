5a567d7fb41a20f3197a33b3a6c2b4307beec9ea
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-29 11:54:38 -0500

    DONE exercise2.sql

--- answers/exercise2.sql
+++ answers/exercise2.sql
@@ -1,3 +1,10 @@
 Write a Query that returns all the Counties by State on separate lines.
 
 SELECT state, county FROM zipcodes ORDER BY state, county;
+
+
+
+FIXING DATA 
+Write a Query that returns the city, state and number of instances of all of the FALSE or NULL timezones.
+
+mysql> SELECT city,state,COUNT(*) AS instance_count FROM zipcodes WHERE timezone IS NULL OR timezone = FALSE  GROUP BY city, state ORDER BY instance_count DESC;
