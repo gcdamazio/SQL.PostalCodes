c6d24e77490f3f26e13d99e6c232f6e49553fedd
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-29 13:22:50 -0500

    DONE exercise5.sql

--- answers/exercise5.sql
+++ answers/exercise5.sql
@@ -1,3 +1,8 @@
 Write a Query that returns all of the individual timezones.
 
 mysql> SELEC DISTINCT timezone FROM zipcodes ORDER BY timezone;
+
+
+Write a Query that locates the zipcode, city, state, and county of all of the timezones that have pipes (|) in them.
+
+mysql> SELECT zip,city,state,county,timezone FROM zipcodes WHERE timezone LIKE '%|%';
