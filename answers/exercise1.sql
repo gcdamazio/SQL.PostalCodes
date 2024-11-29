b9bff9ca749372860aed675bc39cbef9b6520e9f
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-29 11:49:39 -0500

    DONE exercise1.sql

--- answers/exercise1.sql
+++ answers/exercise1.sql
@@ -1,3 +1,8 @@
 Write a Query that returns all the unique state names.
 
 SELECT DISTINCT state FROM zipcodes;
+
+FIXING DATA
+Write a Query that returns the zipcode, city, state, and county of all of the FALSE or NULL timezones.
+
+mysql> SELECT zip,city,state,county FROM zipcodes WHERE timezone IS NULL OR timezone = FALSE;
